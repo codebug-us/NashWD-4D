cc28a3e89a232cd55b0042ffa6396668635d7d01
Ford Chope <douglas.f.chope@vanderbilt.edu>
2019-05-10 12:13:55 -0500

    jq

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){ 
+	console.log("page is loaded");
 	// Your code goes below!
+var title = $("#title");
+title.fadeOut(4000);
+$("#title").css('color', 'green');
 
+var words = $(".word");
+words.hide();
 
+$(".keep").show();
 
+$("#we").fadeTo(4000,.2);
 
+var love = $(".phrase");
+love.hide();
+love.fadeIn(5000);
 
 
 
