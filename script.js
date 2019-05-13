9c82bd033f100d8e866a37d668235af89d9ca0f4
aemiller2013 <alexandra.e.miller@vanderbilt.edu>
2019-05-13 10:05:57 -0500

    completed task

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 $(document).ready(function(){ 
 	// Your code goes below!
+	// alert("All set up!");
+	// var title = $("#title");
+	// console.log(title);
+	// title.fadeOut(400);
+	
+	// //you are smart
+	// // $(".word").hide();
+	// $(".word").fadeTo();
+	// // $("#you, #are, #smart").show();
+	// $("#you, #are, #smart").fadeIn(4000);
+	// // $("#you, #are, #smart").fadeOut(4000);
 
+	//we love code
+	// $(".word").hide();
+	// $(".word").not("#we, #love, #code").fadeTo(400,.2);
+	// $("#we, #love, #code").fadeIn(400);
+	// // $("#we, #love, #code").show();
+	// $("#we, #love, #code").css("color","red");
 
-
-
-
+	//myphrase (we love to eat)
+	$(".word").not("#we, #love, #to, #eat").fadeTo(400,.2);
+	$("#we, #love, #to, #eat").fadeIn(400);
+	$("#we, #love, #to, #eat").css("color","blue");
 
 
 });
